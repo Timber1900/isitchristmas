692dbed934f746a04615a322ddc7acf6db8f3665
Timber1900 <timber.gamer@gmail.com>
2021-12-24 14:23:20 +0000

    fixed sutff

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,114 @@
-if(!self.define){let e,s={};const n=(n,i)=>(n=new URL(n+".js",i).href,s[n]||new Promise((s=>{if("document"in self){const e=document.createElement("script");e.src=n,e.onload=s,document.head.appendChild(e)}else e=n,importScripts(n),s()})).then((()=>{let e=s[n];if(!e)throw new Error(`Module ${n} didn’t register its module`);return e})));self.define=(i,a)=>{const c=e||("document"in self?document.currentScript.src:"")||location.href;if(s[c])return;let t={};const o=e=>n(e,c),r={module:{uri:c},exports:t,require:o};s[c]=Promise.all(i.map((e=>r[e]||o(e)))).then((e=>(a(...e),t)))}}define(["./workbox-1846d813"],(function(e){"use strict";importScripts(),self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"/_next/static/chunks/framework-91d7f78b5b4003c8.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/chunks/main-bc4b0320a9a4651f.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/chunks/pages/_app-7611051f39b2090f.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/chunks/pages/_error-2280fa386d040b66.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/chunks/pages/index-a1345a836ae4547d.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/chunks/polyfills-5cd94c89d3acac5f.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/chunks/webpack-514908bffb652963.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/css/8e7f7f0d89c0d8d0.css",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/iz3gudJ_ouDZ-SOaLCvVd/_buildManifest.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/iz3gudJ_ouDZ-SOaLCvVd/_middlewareManifest.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/_next/static/iz3gudJ_ouDZ-SOaLCvVd/_ssgManifest.js",revision:"iz3gudJ_ouDZ-SOaLCvVd"},{url:"/favicon.png",revision:"151d623791c15be83db4adb9df16b54b"},{url:"/icons/icon-128x128.png",revision:"24ef1587ab4f202e8a4f75c7507f8cc1"},{url:"/icons/icon-144x144.png",revision:"3de430a141dcd12c5a2efd528540e097"},{url:"/icons/icon-152x152.png",revision:"3f69feb1497530459dc6351a14e2e841"},{url:"/icons/icon-16x16.png",revision:"7c28e92f0b2f5841297a8488fa2be545"},{url:"/icons/icon-192x192.png",revision:"e019c4162c524dcdcef2ad364b24c2f4"},{url:"/icons/icon-32x32.png",revision:"1f3e43d28dc2a2d21cc70d5a27c11aff"},{url:"/icons/icon-384x384.png",revision:"50fcf224a113c863d07ae597e46c6ed0"},{url:"/icons/icon-512x512.png",revision:"30e096985ae83f21d22657fe793c8bb4"},{url:"/icons/icon-72x72.png",revision:"43de885bb2fbc42d852217a1f6e0fc6f"},{url:"/icons/icon-96x96.png",revision:"c29b679289aa10719913d4c6f3a0e068"},{url:"/manifest.json",revision:"06e26ca1e74de8fca9199178776d41e6"}],{ignoreURLParametersMatching:[]}),e.cleanupOutdatedCaches(),e.registerRoute("/",new e.NetworkFirst({cacheName:"start-url",plugins:[{cacheWillUpdate:async({request:e,response:s,event:n,state:i})=>s&&"opaqueredirect"===s.type?new Response(s.body,{status:200,statusText:"OK",headers:s.headers}):s}]}),"GET"),e.registerRoute(/^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,new e.CacheFirst({cacheName:"google-fonts-webfonts",plugins:[new e.ExpirationPlugin({maxEntries:4,maxAgeSeconds:31536e3})]}),"GET"),e.registerRoute(/^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,new e.StaleWhileRevalidate({cacheName:"google-fonts-stylesheets",plugins:[new e.ExpirationPlugin({maxEntries:4,maxAgeSeconds:604800})]}),"GET"),e.registerRoute(/\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,new e.StaleWhileRevalidate({cacheName:"static-font-assets",plugins:[new e.ExpirationPlugin({maxEntries:4,maxAgeSeconds:604800})]}),"GET"),e.registerRoute(/\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,new e.StaleWhileRevalidate({cacheName:"static-image-assets",plugins:[new e.ExpirationPlugin({maxEntries:64,maxAgeSeconds:86400})]}),"GET"),e.registerRoute(/\/_next\/image\?url=.+$/i,new e.StaleWhileRevalidate({cacheName:"next-image",plugins:[new e.ExpirationPlugin({maxEntries:64,maxAgeSeconds:86400})]}),"GET"),e.registerRoute(/\.(?:mp3|wav|ogg)$/i,new e.CacheFirst({cacheName:"static-audio-assets",plugins:[new e.RangeRequestsPlugin,new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400})]}),"GET"),e.registerRoute(/\.(?:mp4)$/i,new e.CacheFirst({cacheName:"static-video-assets",plugins:[new e.RangeRequestsPlugin,new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400})]}),"GET"),e.registerRoute(/\.(?:js)$/i,new e.StaleWhileRevalidate({cacheName:"static-js-assets",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400})]}),"GET"),e.registerRoute(/\.(?:css|less)$/i,new e.StaleWhileRevalidate({cacheName:"static-style-assets",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400})]}),"GET"),e.registerRoute(/\/_next\/data\/.+\/.+\.json$/i,new e.StaleWhileRevalidate({cacheName:"next-data",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400})]}),"GET"),e.registerRoute(/\.(?:json|xml|csv)$/i,new e.NetworkFirst({cacheName:"static-data-assets",plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400})]}),"GET"),e.registerRoute((({url:e})=>{if(!(self.origin===e.origin))return!1;const s=e.pathname;return!s.startsWith("/api/auth/")&&!!s.startsWith("/api/")}),new e.NetworkFirst({cacheName:"apis",networkTimeoutSeconds:10,plugins:[new e.ExpirationPlugin({maxEntries:16,maxAgeSeconds:86400})]}),"GET"),e.registerRoute((({url:e})=>{if(!(self.origin===e.origin))return!1;return!e.pathname.startsWith("/api/")}),new e.NetworkFirst({cacheName:"others",networkTimeoutSeconds:10,plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:86400})]}),"GET"),e.registerRoute((({url:e})=>!(self.origin===e.origin)),new e.NetworkFirst({cacheName:"cross-origin",networkTimeoutSeconds:10,plugins:[new e.ExpirationPlugin({maxEntries:32,maxAgeSeconds:3600})]}),"GET")}));
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-9a8b0a38'], (function (workbox) { 'use strict';
+
+  /**
+  * Welcome to your Workbox-powered service worker!
+  *
+  * You'll need to register this file in your web app.
+  * See https://goo.gl/nhQhGp
+  *
+  * The rest of the code is auto-generated. Please don't update this file
+  * directly; instead, make changes to your Workbox build configuration
+  * and re-run your build process.
+  * See https://goo.gl/2aRDsh
+  */
+
+  importScripts();
+  self.skipWaiting();
+  workbox.clientsClaim();
+  workbox.registerRoute("/", new workbox.NetworkFirst({
+    "cacheName": "start-url",
+    plugins: [{
+      cacheWillUpdate: async ({
+        request,
+        response,
+        event,
+        state
+      }) => {
+        if (response && response.type === 'opaqueredirect') {
+          return new Response(response.body, {
+            status: 200,
+            statusText: 'OK',
+            headers: response.headers
+          });
+        }
+
+        return response;
+      }
+    }]
+  }), 'GET');
+  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
+    "cacheName": "dev",
+    plugins: []
+  }), 'GET');
+
+}));
+//# sourceMappingURL=sw.js.map
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from 'react'
 export default function Home() {
   const [isChristmas, setIsChristmas] = useState<boolean | null>(null)
   const [timeMissing, setTimeMissing] = useState<string | null>(null)
+  const [day, setDay] = useState<number>();
 
   useEffect(() => {
     const date = new Date()
     const month = date.getMonth()
     const day = date.getDate()
+    setDay(day)
 
     if(month === 11 && (day === 25 || day === 24)) {
       setIsChristmas(true)
@@ -37,8 +39,11 @@ export default function Home() {
     const minutes = Math.floor(timeDiff / (1000 * 60)) - months * 30 * 24 * 60 - days * 24 * 60 - hours * 60;
     const seconds = Math.floor(timeDiff / 1000) - months * 30 * 24 * 60 * 60 - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
 
-    if((days === 0 && months === 0) || (days == 29 && months === 11)) {
+    if(days == 29 && months === 11) {
       return "It's Christmas!! 🎉🎉🎉"
+    } else if(days === 0 && months === 0) {
+      return `It's Christmas eve!! 🎉🎉🎉
+      ${months} months, ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`
     }
 
     return `${months} months, ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`
@@ -46,8 +51,8 @@ export default function Home() {
 
   return (
     <main className={`grid w-screen h-screen ${isChristmas ? "bg-green-600" :"bg-red-600"} place-content-center`}>
-        <h1 className="w-screen font-sans text-3xl font-bold text-center text-white sm:text-9xl">{isChristmas ? 'Yes!' : 'No.'}</h1>
-        <p className="w-screen h-auto px-4 font-sans text-xl font-bold text-center text-white sm:text-3xl">{timeMissing}</p>
+        <h1 className="w-screen font-sans text-3xl font-bold text-center text-white sm:text-9xl">{isChristmas ? day === 25 ? 'Yes!' : 'Yes!*' : 'No.'}</h1>
+        <p className="w-screen h-auto px-4 font-sans text-xl font-bold text-center text-white whitespace-pre-wrap sm:text-3xl">{timeMissing}</p>
     </main>
   )
 }
